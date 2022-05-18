696f673690c907eeb05e72ddc58262b49511e424
byeongdae yu <brianbdyu@gmail.com>
2022-05-17 23:41:22 -0400

    commit tree 3998187d4dade42ef7947ed15c55035088c7aaef

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
+
+/*describe('index.js', function() {
+    describe('multiply', function() {
+      it("is an equation whose multiplied results will equal 62", function() {
+        expect(num1).to.be.a('number')
+        expect(num2).to.be.a('number')
+        expect(multiply).to.eq(62)
+      });
+    });
+    describe('random', function() {
+      it("generates a random integer greater than 0", function() {
+        expect(Number.isInteger(random)).to.be.true
+        expect(random).to.not.eq(0)
+      });
+    });
+    describe('mod', function() {
+      it("is an equation that calculates a remainder that is equal to 4", function() {
+        expect(num3).to.be.a('number')
+        expect(num4).to.be.a('number')
+        expect(mod).to.eq(4)
+      });
+    });
+    describe('max', function() {
+      it("will return 20 as the highest number in the set", function() {
+        expect(max).to.eq(20)
+      });
+    });
+  });
+  */
